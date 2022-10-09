025a10812e11e4d82ccfc98ae3df42b243c4a172
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2022-10-10 00:25:12 +0200

    first try

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,32 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'persons',
+    loadChildren: () => import('./pages/persons/persons.module').then( m => m.PersonsPageModule)
+  },
+  {
+    path: 'tasks',
+    loadChildren: () => import('./pages/tasks/tasks.module').then( m => m.TasksPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'assigned-tasks',
+    loadChildren: () => import('./pages/viewtasks/assigned-tasks.module').then( m => m.AssignedTasksPageModule)
+  },
+  {
+    path: 'assign-tasks',
+    loadChildren: () => import('./pages/assign-tasks/assign-tasks.module').then( m => m.AssignTasksPageModule)
   },
 ];
 
