be7a60beb3d48e192813d2a3ef8da727abf496fc
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2022-11-18 10:11:22 +0100

    commit

--- src/app/pages/assign-tasks/components/assignment/assignment.component.spec.ts
+++ src/app/pages/assign-tasks/components/assignment/assignment.component.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { AssignementComponent } from './assignement.component';
+import { AssignmentComponent } from './assignment.component';
 
-describe('AssignementComponent', () => {
-  let component: AssignementComponent;
-  let fixture: ComponentFixture<AssignementComponent>;
+describe('AssignmentComponent', () => {
+  let component: AssignmentComponent;
+  let fixture: ComponentFixture<AssignmentComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ AssignementComponent ],
+      declarations: [ AssignmentComponent ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(AssignementComponent);
+    fixture = TestBed.createComponent(AssignmentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
